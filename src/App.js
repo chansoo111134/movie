b5f441e90717aa433553a0e738a4e990e6a71c2d
AI18 <chinggo3342@naver.com>
2022-09-28 19:12:08 +0900

    220924

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ProTypes from 'prop-types'
+
+function Food({name, picture, rating}) {
+  return (
+    <div>
+      <h1>엄({name})</h1>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+
+  );
+}
+
+const foodILike = [
+{
+  id: 1,
+  name: '김치',
+  image : 'http://www.realkimchi.com/shopimages/realkimchi/001001000001.jpg?1496886866',
+  rating: 5,
+},
+{
+ id:2,
+ name: '삼겹살',
+ image : 'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg',
+ rating: 4.9,
+}
+];
+
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>성법</h1>
+      {foodILike.map( dish => (<Food key={dish.id}
+      name={dish.name} picture={dish.image} rating={dish.rating}/>))
     </div>
   );
 }
+
+
+Food.proTypes ={
+  name:ProTypes.string.isRequired,
+  picture:ProTypes.string.isRequired,
+  rating:ProTypes.number.isRequired,
+}
+
+
+
+
 
 export default App;
