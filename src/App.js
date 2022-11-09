72326f3b632defad11e242623872829636ca9300
AI18 <chinggo3342@naver.com>
2022-11-09 19:17:51 +0900

    1109

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
 import React from 'react';
+import './App.css';
+import {HashRouter, Route} from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
 
-class App extends React.Component
-{
-  constructor(props) {
-    super(props);
-    console.log('집가고싶은');
-  }
-
-  componentDidCatch() {
-    console.log("집 박고싶은");
-  }
-  state ={
-    count: 0
-  };
-
-  
-  add = () => {
-    console.log('add');
-    this.setState(current => ({count: current.count +1}));
-  }
-  
-  minus = () => {
-    console.log('minus');
-    this.setState({count: this.state.count -1});
-  }
-
-  reset = () => {
-    console.log('reset');
-    this.setState({count: 0});
-  }
-
- 
-  render() 
-  {
-  console.log('render');
-  return ( 
-  <div>
-    <h1> Class Component {this.state.count} </h1>;
-    <button onClick={this.add}> Add </button>
-    <button onClick={this.minus}> Minus </button>
-    <button onClick={this.reset}> reset </button>
-  </div>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <React path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+    </HashRouter>
+  );
 }
 
-
-
 export default App;
+
+
+
+
+
+
